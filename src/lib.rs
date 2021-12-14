44d2efad601e50447ad73cb519c150e231dca8ae
Denzyl Dick <denzyl@live.nl>
2021-12-14 19:06:15 +0100

    Find lines

--- src/lib.rs
+++ src/lib.rs
@@ -19,7 +19,46 @@ impl Config {
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
-    println!("{}", contents);
 
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "helloworld";
+        let contents = "\
+  Lorem Ipsum is simply dummy
+  text of the printing and typesetting industry.
+  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown
+  printer took a galley of type and scrambled it to
+  make a type helloworld
+  specimen book.
+  It has survived not only five centuries, but also the leap into electronic
+  typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of
+  Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing
+   software like Aldus PageMaker including versions of Lorem Ipsum.
+    ";
+
+        assert_eq!(vec![
+            "  make a type helloworld"
+        ],
+                   search(query, contents))
+    }
+}
+
+fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut result = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            result.push(line)
+        }
+    }
+    return result;
 }
